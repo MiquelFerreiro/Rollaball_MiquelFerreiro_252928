854ed070584c1565749895cf4e7582aa4ae3a293
MiquelFerreiro <miquel.ferreiro01@estudiant.upf.edu>
2024-04-15 22:06:06 +0200

    Final

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,33 @@ public class PlayerController : MonoBehaviour
     public float speed = 10;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public GameObject perfectTextObject;
+    public AudioClip pickupSound;
+    public AudioClip winSound;
+    public AudioClip perfectSound;
 
     private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int total;
+    private bool extra;
+    private AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        total = 12;
+        extra = false;
 
         SetCountText();
         winTextObject.SetActive(false);
+        perfectTextObject.SetActive(false);
+
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     void OnMove(InputValue movementValue)
@@ -35,18 +48,26 @@ public class PlayerController : MonoBehaviour
     }
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        countText.text = "Count: " + count.ToString() + " / " + total.ToString();
+
+        if(count == total && extra)
+        {
+            winTextObject.SetActive(false);
+            perfectTextObject.SetActive(true);
+
+            audioSource.PlayOneShot(perfectSound);
+        }
+        else if(count == total)
         {
             winTextObject.SetActive(true);
+
+            audioSource.PlayOneShot(winSound);
         }
     }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        //movement = new Vector3(1, 0.0f, 1);
 
         rb.AddForce(movement * speed);
 
@@ -57,9 +78,23 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
-            count = count + 1;
+            count += 1;
 
             SetCountText();
+
+            audioSource.PlayOneShot(pickupSound);
+        }
+        else if (other.gameObject.CompareTag("Extra PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            extra = true;
+            count += 1;
+            total += 1;
+
+            countText.color = Color.red;
+            SetCountText();
+
+            audioSource.PlayOneShot(pickupSound);
         }
     }
 }
